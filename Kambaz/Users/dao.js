73ac2c0eaae848dbb63f0bd039933407c12e6ab6
K K <guopeng.ee@gmail.com>
2025-06-16 22:55:25 -0700

    a6

--- Kambaz/Users/dao.js
+++ Kambaz/Users/dao.js
@@ -1,41 +1,22 @@
-import db from "../Database/index.js";
+import model from "./model.js";
 import { v4 as uuidv4 } from "uuid";
 
-// Get users array from database
-let { users } = db;
-
-// Create a new user with generated ID
 export const createUser = (user) => {
-  const newUser = { ...user, _id: uuidv4() };
-  users = [...users, newUser];
-  return newUser;
-};
-
-// Find all users
-export const findAllUsers = () => users;
-
-// Find user by ID
-export const findUserById = (userId) => 
-  users.find((user) => user._id === userId);
-
-// Find user by username
-export const findUserByUsername = (username) => 
-  users.find((user) => user.username === username);
-
-// Find user by credentials (for login)
-export const findUserByCredentials = (username, password) =>
-  users.find(
-    (user) => user.username === username && user.password === password
-  );
-
-// Update existing user
-export const updateUser = (userId, user) => {
-  users = users.map((u) => (u._id === userId ? user : u));
-  return users.find((u) => u._id === userId);
-};
-
-// Delete user
-export const deleteUser = (userId) => {
-  users = users.filter((u) => u._id !== userId);
-  return users;
-};
+    const newUser = { ...user, _id: uuidv4() }; // NEW
+    return model.create(newUser);
+}
+  
+export const findAllUsers = () => model.find();
+export const findUserById = (userId) => model.findById(userId);
+export const findUserByUsername = (username) =>  model.findOne({ username: username });
+export const findUserByCredentials = (username, password) =>  model.findOne({ username, password });
+export const updateUser = (userId, user) =>  model.updateOne({ _id: userId }, { $set: user });
+export const deleteUser = (userId) => model.deleteOne({ _id: userId });
+export const findUsersByRole = (role) => model.find({ role: role }); // or just model.find({ role })
+export const findUsersByPartialName = (partialName) => {
+    const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
+    return model.find({
+      $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }],
+    });
+  };
+  
--- index.js
+++ index.js
@@ -11,6 +11,10 @@ import EnrollmentRoutes from "./Kambaz/Enrollments/routes.js";
 
 import cors from 'cors';
 
+import mongoose from "mongoose";
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+mongoose.connect(CONNECTION_STRING);
+
 const app = express();
 
 
